4d91c631d0b423959d261ed4f12184eb98c91178
Robert Fratto <robertfratto@gmail.com>
2017-08-18 20:57:23 -0400

    Add CMake support.

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # FreeImage 
 
-**NOTE:** This project has only just been imported and does not yet support CMake builds. Stay tuned for a later commit that will have CMake support.
-
 This repository is a fork of the FreeImage project that supports building via CMake. Please see the [original project](https://sourceforge.net/projects/freeimage/) if you want the unmodified codebase.
+
+## Building 
+
+Make sure CMake is installed, and run the following commands:
+
+```
+$ mkdir build-FreeImage 
+$ cmake /path/to/FreeImage 
+$ make 
+```
--- Source/LibJXR/common/include/guiddef.h
+++ Source/LibJXR/common/include/guiddef.h
@@ -1,14 +1,14 @@
 //+---------------------------------------------------------------------------
 //
-// Copyright © Microsoft Corp.
+// Copyright ï¿½ Microsoft Corp.
 // All rights reserved.
 // 
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
 // 
-// • Redistributions of source code must retain the above copyright notice,
+// ï¿½ Redistributions of source code must retain the above copyright notice,
 //   this list of conditions and the following disclaimer.
-// • Redistributions in binary form must reproduce the above copyright notice,
+// ï¿½ Redistributions in binary form must reproduce the above copyright notice,
 //   this list of conditions and the following disclaimer in the documentation
 //   and/or other materials provided with the distribution.
 // 
@@ -87,7 +87,7 @@ typedef struct _GUID {
                 = { l, w1, w2, { b1, b2,  b3,  b4,  b5,  b6,  b7,  b8 } }
 #else
 #define DEFINE_GUID(name, l, w1, w2, b1, b2, b3, b4, b5, b6, b7, b8) \
-    EXTERN_C const GUID FAR name
+	EXTERN_C const GUID name
 #endif // INITGUID
 
 #define DEFINE_OLEGUID(name, l, w1, w2) DEFINE_GUID(name, l, w1, w2, 0xC0,0,0,0,0,0,0,0x46)
